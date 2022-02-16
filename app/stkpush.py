b619fc210baabf3ddd81e4f705ea9c2f7c86b89c
DanielNduati <dannduati2@gmail.com>
2022-02-16 22:26:18 +0300

    chore: add stkpush module
    Mpesa Express Api initiates online payment on behalf of a customer.This
    module consumes the api by sending post a request with the json encoded data
    necessary as per the documentation. I use httpx an asyncio-based library to
    make requests asynchronously.

---
+++ app/stkpush.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import httpx
+from utils import generate_password
+# post request
+
+
+async def initiate_payment(url: str, access_token: str, busines_shortcode: int, lnm_passkey: str, timestamp: str, amount: int, phone_number: int, callback_url: str):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}"
+    }
+    # json data
+    payload = {
+        "BusinessShortCode": busines_shortcode,
+        "Password": generate_password(busines_shortcode, lnm_passkey),
+        "Timestamp": timestamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": 1,
+        "PartyA": phone_number,
+        "PartyB": busines_shortcode,
+        "PhoneNumber": phone_number,
+        "CallBackURL": callback_url,
+        "AccountReference": "CompanyXLTD",
+        "TransactionDesc": "Payment of X"
+    }
+
+    async with httpx.AsyncClient() as client:
+        r = await client.post(url, headers=headers, json=payload)
+    return r.json()
