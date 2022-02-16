72e71b341b83c229e36e8accf49a58a9dafa9bc6
DanielNduati <dannduati2@gmail.com>
2022-02-16 22:30:34 +0300

    update: add stkpush path operation function

--- app/main.py
+++ app/main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
 from accessToken import getAcessToken
+from stkpush import initiate_payment
 from config import settings
+from utils import get_timestamp, generate_password
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello There!"}
+    print(generate_password(settings.business_shortcode, settings.lnm_passkey))
+    return {"message": "Hello"}
 
 
-@app.get("/access_token")
-async def Token() -> str:
-    # make the request
-    at = await getAcessToken(settings.accesstoken_url, settings.consumer_key, settings.consumer_secret)
-    return at["access_token"]
+@app.get("/stk_push")
+async def stkPush():
+    # get access token
+    at = await getAcessToken(settings.accesstoken_url,
+                             settings.consumer_key, settings.consumer_secret)
+    # initate stkpush
+    r = await initiate_payment(settings.lnm_url, at["access_token"], settings.business_shortcode, settings.lnm_passkey, get_timestamp(), 1, 254723060846, settings.lnm_callback_url)
+    print("done")
+    return r
