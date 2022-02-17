f5010e458b7d56b5306b44763c50d9bcea21b777
DanielNduati <dannduati2@gmail.com>
2022-02-17 23:37:35 +0300

    chore: add database models module
    Create database models that inherit from the base class in the database
    module. Add model class attributes for each table in this case the users
    and the units table.

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    phonenumber = Column(Integer, nullable=False)
+
+
+class Unit(Base):
+    __tablename__ = "units"
+    id = Column(Integer, primary_key=True, index=True)
+    value = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    owner = relationship("User")
