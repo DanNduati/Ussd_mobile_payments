704e4ebbc1d2d3dc621fcf4116f1afdbe60a0669
DanielNduati <dannduati2@gmail.com>
2022-02-17 23:44:29 +0300

    chore: update database models module
    Create a one to many relationship between the two tables

--- app/models.py
+++ app/models.py
@@ -8,11 +8,11 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     phonenumber = Column(Integer, nullable=False)
-
+    units = relationship("Unit", back_populates="owner")
 
 class Unit(Base):
     __tablename__ = "units"
     id = Column(Integer, primary_key=True, index=True)
     value = Column(Integer, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    owner = relationship("User")
+    owner = relationship("User", back_populates="units")
