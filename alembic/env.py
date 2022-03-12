d14763a61333ac50b4bd6b182ab2a7968cd91285
DanielNduati <dannduati2@gmail.com>
2022-03-12 08:15:35 +0300

    Configure alembic migration environment

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,9 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
-
+from app.models import Base
+from app.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -12,12 +11,13 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
-
+#override the sqlalchemy database url
+config.set_main_option("sqlalchemy.url",settings.sqlalchemy_database_uri)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
