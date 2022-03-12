16e71a5b9529bc6dc2b61aa197a330729196620c
DanielNduati <dannduati2@gmail.com>
2022-03-12 04:48:54 +0300

    Update accesstoken module
    Return access token itself not the whole json request response

--- app/accessToken.py
+++ app/accessToken.py
@@ -4,5 +4,6 @@ import httpx
 async def getAcessToken(url: str, consumer_key: str, consumer_secret: str):
     # make the request
     async with httpx.AsyncClient() as client:
-        r = await client.get(url, auth=(consumer_key, consumer_secret))
-    return r.json()
+        response = await client.get(url, auth=(consumer_key, consumer_secret))
+        access_token = response.json()["access_token"]
+    return access_token
