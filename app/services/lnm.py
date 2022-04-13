9646b950516bb4b90a234778dfe32dda0e7047be
DanielNduati <dannduati2@gmail.com>
2022-04-13 17:43:32 +0300

    Add stk push initiation to lnm service

--- app/services/lnm.py
+++ app/services/lnm.py
@@ -1,6 +1,8 @@
 import httpx
-from utils.lnm import generate_password
+from utils.lnm import generate_password, get_timestamp
+from config import settings
 # post request
+
 
 async def getAcessToken(url: str, consumer_key: str, consumer_secret: str):
     # make the request
@@ -8,6 +10,7 @@ async def getAcessToken(url: str, consumer_key: str, consumer_secret: str):
         response = await client.get(url, auth=(consumer_key, consumer_secret))
         access_token = response.json()["access_token"]
     return access_token
+
 
 async def initiate_payment(url: str, access_token: str, busines_shortcode: int, lnm_passkey: str, timestamp: str, amount: int, phone_number: int, callback_url: str):
     headers = {
@@ -32,3 +35,11 @@ async def initiate_payment(url: str, access_token: str, busines_shortcode: int, 
     async with httpx.AsyncClient() as client:
         r = await client.post(url, headers=headers, json=payload)
     return r.json()
+
+
+async def send_stk(amount: int):
+    # get access token
+    access_token = await getAcessToken(settings.accesstoken_url, settings.consumer_key, settings.consumer_secret)
+    timestamp = get_timestamp()
+    # initate stkpush
+    r = await initiate_payment(settings.lnm_url, access_token, settings.business_shortcode, settings.lnm_passkey, timestamp, amount, 254723060846, settings.lnm_callback_url)
