9fbf463b308ed80eeb12cc9770f599c2f10906b3
DanielNduati <dannduati2@gmail.com>
2022-04-13 17:47:03 +0300

    Add callback response data to file and clean imports

--- app/routers/lnm.py
+++ app/routers/lnm.py
@@ -1,6 +1,5 @@
-from typing import List,Optional
-from fastapi import APIRouter,Request
-from fastapi.responses import HTMLResponse
+import json
+from fastapi import APIRouter, Request
 from pydantic import BaseModel, Json
 
 router = APIRouter(
@@ -8,10 +7,24 @@ router = APIRouter(
     tags=["Payment Callback"]
 )
 
-class Payment(BaseModel):
-    pass
 
 @router.post("/confirmation")
 async def process_payment(request: Request):
-    print(f"request json {await request.json()}")
-    return {"Message":"Callback hit!"}
+    payment_response = await request.json()
+    # print(payment_response)
+    # serialize the callback response
+
+    json_dump = json.dumps(payment_response, indent=4)
+    json_object = json.loads(json_dump)
+    # print(json_object["Body"]["stkCallback"]["ResultCode"])
+    if json_object["Body"]["stkCallback"]["ResultCode"] == 0:
+        # successful payment store payment data to file later db
+        print(json_object["Body"]["stkCallback"]["ResultDesc"])
+        with open("data/data.json", "w") as outfile:
+            json.dump(payment_response, outfile)
+    else:
+        # unsuccessful payement Todo implement payment logging
+        print(json_object["Body"]["stkCallback"]["ResultDesc"])
+        with open("data/unsuccessful.json", "w") as outfile:
+            json.dump(payment_response, outfile)
+    return {"Message": "Callback received"}
