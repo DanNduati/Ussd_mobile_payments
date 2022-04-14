c36b9ea9bf91ec81126ea8613fab1b4cac9fb226
DanielNduati <dannduati2@gmail.com>
2022-04-14 20:55:29 +0300

    Update ussd router
    Add top level ussd menu with 'dynamic greeting' lol
    Query user from phone number from AT ussd callback response
    Diifferent menu for users in db and those not in db

--- app/routers/ussd.py
+++ app/routers/ussd.py
@@ -1,3 +1,4 @@
+import datetime
 from fastapi import APIRouter, Form
 from fastapi.responses import HTMLResponse
 from typing import Optional
@@ -14,41 +15,54 @@ router = APIRouter(
 )
 
 
-async def process_response(resp: str, phone_number: str):
-    response: str = ""
-    # get user by phonenumber and send greeting
-    # print(resp)
+def generate_greeting():
+    hour = datetime.datetime.now().hour
+    greeting = "Good morning" if 5 <= hour < 12 else "Good afternoon" if hour < 18 else "Good evening"
+    return greeting
 
-    # user = await ussd.get_user(phone_number)
-    user = {
-        "name": "Daniel"
-    }
-    #print(f"user: {user}")
+
+async def process_response(resp: str, user):
+    response: str = ""
     if len(resp) <= 1:
+        # top level menu
         if resp == "":
+            # The first request.
             if user:
-                response = f"CON Hello {user['name']}\n"
-                response += "1. Query Bill \n"
-                response += "2. Exit"
+                response = f"CON {generate_greeting()} {user.name} please select an option\n"
+                response += "1. query Bill \n"
+                response += "2. exit"
             else:
-                response = f"CON Hello!"
-                response += "1. Query Bill \n"
-                response += "2. Exit"
+                response = f"CON {generate_greeting()} please select an option\n"
+                response += "1. query Bill \n"
+                response += "2. exit"
         elif resp == "1":
-            response = "CON Feature to be implemented"
+            if user:
+                response = "CON Choose the unit account\n"
+                for i, unit in enumerate(user.units):
+                    response += f"{i+1}. unitid{user.units[i].id}\n"
+                response += f"{len(user.units)+1}. enter a different unit id\n"
+                response += f"{len(user.units)+2}. exit\n"
+            else:
+                response = "CON Please enter the unit id for your account\n"
+            #response += f"UnitID:{user.units}\n"
         elif resp == "2":
             response = "END Thank you for using this service!"
+    elif len(resp) == 3:
+        pass
+    else:
+        pass
     return response
 
 
 @router.post("/callback", response_class=HTMLResponse)
-async def ussd_callback(session_id: str = Form(""), service_code: str = Form(""), phone_number: str = Form(""), text: Optional[str] = Form("")):
+async def ussd_callback(sessionId: str = Form(""), serviceCode: str = Form(""), phoneNumber=Form(""), text: Optional[str] = Form(""), db: Session = Depends(get_db)):
     # get user inside route
-    print(f"phone_no: {phone_number}")
+    print(f"session_id: {sessionId}")
+    print(f"service_code: {serviceCode}")
+    print(f"phone_no: {phoneNumber}")
     print(f"text: {text}")
-    """
+    phone_number: str = phoneNumber.replace("+", "")
     user = db.query(models.User).filter(
         models.User.phonenumber == phone_number).first()
-    print(user)
-    """
-    return await process_response(text, phone_number)
+
+    return await process_response(text, user=user)
